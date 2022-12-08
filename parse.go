d8e0359c1236cfde6354dda59571245ef781aa65
Nic Klaassen <nic@goteleport.com>
2022-12-08 10:28:40 -0800

    feat: support binary expressions as function arguments (#8)
    The title of this commit is a bit of a simplification - not only are binary expressions now supported as function arguments, but unary, binary, and paren expressions are now supported at all levels of the parse tree. This is mostly relevant to function arguments and index expressions.
    
    Previously, `(*predicateParser).parse` and `(*predicateParser).evaluateExpr` were separate functions. Unary, binary, and paren expressions were parsed at the top level in `parse`, and evaluation would be handed off to `evaluateExpr` whenever a different type of expression was encountered. Notably, `evaluateExpr` would never call back out to `parse`. This lead to the fact that any unary, binary, or paren expressions within any other type of expression could never be parsed.
    
    The change here is to combine `evaluateExpr` and `parse` into a single function - `parse`. Now all of the expression types are part of the same switch statement. Where previously function arguments were only handled by `evaluateExpr`, they are now handled by parse, and all supported expression types can be handled at all levels.

--- parse.go
+++ parse.go
@@ -32,43 +32,102 @@ func (p *predicateParser) Parse(in string) (interface{}, error) {
 func (p *predicateParser) parse(expr ast.Expr) (interface{}, error) {
 	switch n := expr.(type) {
 	case *ast.BinaryExpr:
-		x, err := p.parse(n.X)
-		if err != nil {
-			return nil, err
-		}
-
-		y, err := p.parse(n.Y)
-		if err != nil {
-			return nil, err
-		}
-
-		return p.joinPredicates(n.Op, x, y)
+		val, err := p.parseBinaryExpr(n)
+		return val, trace.Wrap(err)
 
 	case *ast.ParenExpr:
-		return p.parse(n.X)
+		val, err := p.parse(n.X)
+		return val, trace.Wrap(err)
 
 	case *ast.UnaryExpr:
-		joinFn, err := p.getJoinFunction(n.Op)
-		if err != nil {
-			return nil, err
-		}
+		val, err := p.parseUnaryExpr(n)
+		return val, trace.Wrap(err)
 
-		node, err := p.parse(n.X)
-		if err != nil {
-			return nil, err
-		}
+	case *ast.BasicLit:
+		val, err := literalToValue(n)
+		return val, trace.Wrap(err)
 
-		return callFunction(joinFn, []interface{}{node})
+	case *ast.IndexExpr:
+		val, err := p.parseIndexExpr(n)
+		return val, trace.Wrap(err)
+
+	case *ast.SelectorExpr:
+		val, err := p.parseSelectorExpr(n)
+		return val, trace.Wrap(err)
+
+	case *ast.Ident:
+		if p.d.GetIdentifier == nil {
+			return nil, trace.NotFound("%v is not defined", n.Name)
+		}
+		val, err := p.d.GetIdentifier([]string{n.Name})
+		return val, trace.Wrap(err)
+
+	case *ast.CallExpr:
+		val, err := p.parseCallExpr(n)
+		return val, trace.Wrap(err)
 
 	default:
-		return p.evaluateExpr(n)
+		return nil, trace.BadParameter("%T is not supported", expr)
 	}
+}
+
+func (p *predicateParser) parseBinaryExpr(expr *ast.BinaryExpr) (interface{}, error) {
+	x, err := p.parse(expr.X)
+	if err != nil {
+		return nil, err
+	}
+
+	y, err := p.parse(expr.Y)
+	if err != nil {
+		return nil, err
+	}
+
+	val, err := p.joinPredicates(expr.Op, x, y)
+	return val, trace.Wrap(err)
+}
+
+func (p *predicateParser) parseUnaryExpr(expr *ast.UnaryExpr) (interface{}, error) {
+	joinFn, err := p.getJoinFunction(expr.Op)
+	if err != nil {
+		return nil, err
+	}
+
+	node, err := p.parse(expr.X)
+	if err != nil {
+		return nil, err
+	}
+
+	val, err := callFunction(joinFn, []interface{}{node})
+	return val, trace.Wrap(err)
+}
+
+func (p *predicateParser) parseIndexExpr(expr *ast.IndexExpr) (interface{}, error) {
+	if p.d.GetProperty == nil {
+		return nil, trace.NotFound("properties are not supported")
+	}
+
+	mapVal, err := p.parse(expr.X)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	keyVal, err := p.parse(expr.Index)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	val, err := p.d.GetProperty(mapVal, keyVal)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return val, nil
 }
 
 func (p *predicateParser) evaluateArguments(nodes []ast.Expr) ([]interface{}, error) {
 	out := make([]interface{}, len(nodes))
 	for i, n := range nodes {
-		val, err := p.evaluateExpr(n)
+		val, err := p.parse(n)
 		if err != nil {
 			return nil, trace.Wrap(err)
 		}
@@ -77,81 +136,21 @@ func (p *predicateParser) evaluateArguments(nodes []ast.Expr) ([]interface{}, er
 	return out, nil
 }
 
-func (p *predicateParser) evaluateExpr(n ast.Expr) (interface{}, error) {
-	switch l := n.(type) {
-	case *ast.BasicLit:
-		val, err := literalToValue(l)
-		if err != nil {
-			return nil, err
-		}
-
-		return val, nil
-
-	case *ast.IndexExpr:
-		if p.d.GetProperty == nil {
-			return nil, trace.NotFound("properties are not supported")
-		}
-
-		mapVal, err := p.evaluateExpr(l.X)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-
-		keyVal, err := p.evaluateExpr(l.Index)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-
-		val, err := p.d.GetProperty(mapVal, keyVal)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-
-		return val, nil
-
-	case *ast.SelectorExpr:
-		fields, err := evaluateSelector(l, []string{})
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-
-		if p.d.GetIdentifier == nil {
-			return nil, trace.NotFound("%v is not defined", strings.Join(fields, "."))
-		}
-
-		val, err := p.d.GetIdentifier(fields)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-		return val, nil
-
-	case *ast.Ident:
-		if p.d.GetIdentifier == nil {
-			return nil, trace.NotFound("%v is not defined", l.Name)
-		}
-
-		val, err := p.d.GetIdentifier([]string{l.Name})
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-		return val, nil
-
-	case *ast.CallExpr:
-		fn, args, err := p.getFunctionAndArgs(l)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-
-		arguments, err := p.evaluateArguments(args)
-		if err != nil {
-			return nil, err
-		}
-
-		return callFunction(fn, arguments)
-
-	default:
-		return nil, trace.BadParameter("%T is not supported", n)
+func (p *predicateParser) parseSelectorExpr(expr *ast.SelectorExpr) (interface{}, error) {
+	fields, err := evaluateSelector(expr, []string{})
+	if err != nil {
+		return nil, trace.Wrap(err)
 	}
+
+	if p.d.GetIdentifier == nil {
+		return nil, trace.NotFound("%v is not defined", strings.Join(fields, "."))
+	}
+
+	val, err := p.d.GetIdentifier(fields)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return val, nil
 }
 
 // evaluateSelector recursively evaluates the selector field and returns a list
@@ -169,6 +168,21 @@ func evaluateSelector(sel *ast.SelectorExpr, fields []string) ([]string, error) 
 	default:
 		return nil, trace.BadParameter("unsupported selector type: %T", l)
 	}
+}
+
+func (p *predicateParser) parseCallExpr(expr *ast.CallExpr) (interface{}, error) {
+	fn, args, err := p.getFunctionAndArgs(expr)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	arguments, err := p.evaluateArguments(args)
+	if err != nil {
+		return nil, err
+	}
+
+	val, err := callFunction(fn, arguments)
+	return val, trace.Wrap(err)
 }
 
 func (p *predicateParser) getFunction(name string) (interface{}, error) {
